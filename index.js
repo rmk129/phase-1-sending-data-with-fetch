cccb734748f484f122fc2147fb05012eba76a760
Romil <romil.m.kothari@gmail.com>
2023-04-04 19:25:19 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+function submitData (sname, semail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: sname,
+            email: semail,
+        })
+        
+    })
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function(e) {
+        document.body.innerHTML = e['id']
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+    
+}
+
+
+
+
+// function fetchPost (idObj) {
+    
+//     return fetch('http://localhost:3000/users', {
+//         method: 'POST', 
+//         headers: {
+//             'Content-Type': 'application.json',
+//             "Accept": "application/json",
+//         },
+//         body: JSON.stringify(idObj),
+//     })
+//     .then(resp => resp.json())
+// }
